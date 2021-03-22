82cf2ac5a6b1032401f456d02632c27dbcf65dcf
cybergoat9152 <arthurtbarros@outlook.com>
2021-03-22 18:20:47 -0300

    criando arquivos de projeto

---
+++ trabalhos/Atividade01/src/barros/torres/arthur/Contas.java
@@ -0,0 +1,6 @@
+/*
+Nas contas deve existir informações sobre a mesma:
+-idConta– esse id deveser único para cada conta , podeestar relacionado com o total de contas já criadas.
+- saldo
+- e outros que você julgar necessário para a classe
+ */
