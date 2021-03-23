6914273283ef266f1eca3711da2cb3d3f830de3f
cybergoat9152 <arthurtbarros@outlook.com>
2021-03-23 16:11:29 -0300

    começo da implementação da classe Usuarios

--- trabalhos/Atividade01/src/barros/torres/arthur/Usuarios.java
+++ trabalhos/Atividade01/src/barros/torres/arthur/Usuarios.java
@@ -2,11 +2,42 @@ package barros.torres.arthur;
 
 public class Usuarios {
     
-    /*
-    Asinformaçõesqueaclassequecontéminformaçõesdosusuários deve conter são:
-    - nome
-    - senha
-    - e-mail
-    - e outros que você julgar necessário para a classe
-    */
+    private String name;
+    private String cpf;
+    private String pass;
+    private String email;
+
+    public Usuarios(String name, String cpf, String pass, String email){
+        this.name = name;
+        this.cpf = cpf.replace(".", "").replace("-", ""); // retirando os pontos e digito do cpf
+        this.pass = pass;
+        this.email = email;
+        // Account validation
+        /*--------------------------------colocar metod de verificar erro--------------------------------------*
+    }
+
+    /* Metodos para mudar atributos */
+    public void mudarNome(String name){
+        this.name = name; 
+    }
+    public void mudarCPF(String cpf){
+        this.cpf = cpf;
+    }
+    public void mudarEmail(String email){
+        this.email = email;
+    }
+    public void mudarPass(String pass){
+        this.pass = pass;
+    }
+    /* Metodos validacao */
+    private boolean _validaCPF(){
+        //TODO usuarios.java - metodo de validação
+        return true;
+    }
+    /* Metodo para */
+    public String help(){
+        //TODO terminar a descricao da classe
+        return "\n\n\n\nClasse para criacao de novo usuario.\n\tconstrutor: Usuarios(nome, cpf, pass, email):\n\t\t-nome (String)\n\t\t-cpf (String)\n\t\t-pass (String)\n\t\t-email (String)\n\n\n\n";
+    }
+
 }
--- trabalhos/Atividade01/src/barros/torres/arthur/atividade01.java
+++ trabalhos/Atividade01/src/barros/torres/arthur/atividade01.java
@@ -1,5 +1,9 @@
 package barros.torres.arthur;
 
 public class atividade01 {
-    
+    static Usuarios c1 = new Usuarios("all migth","443.055.508-73","dasdasdas@gmil.com","batata");
+    public static void main(String[] args){
+        System.out.println( c1.help() );
+        
+    }
 }
