535a5b3741771e9be95f039f036a3d1c545fa57c
cybergoat9152 <arthurtbarros@outlook.com>
2021-04-04 15:22:32 -0300

    modelando classe contas

--- trabalhos/Atividade01/src/barros/torres/arthur/Contas.java
+++ trabalhos/Atividade01/src/barros/torres/arthur/Contas.java
@@ -1,6 +1,67 @@
-/*
-Nas contas deve existir informações sobre a mesma:
--idConta– esse id deveser único para cada conta , podeestar relacionado com o total de contas já criadas.
-- saldo
-- e outros que você julgar necessário para a classe
- */
+import java.security.MessageDigest;
+
+public class Contas{
+    /* ----------------------------------------------atributos---------------------------------------------- */
+
+    private String idconta;
+    private double saldo;
+    private Usuarios usuario;
+  
+    /* ----------------------------------------------construtor--------------------------------------------- */
+
+    public Contas(Usuarios usuario){
+        this.idconta = sha256(usuario.consultarCPF()) ;
+        this.saldo = 0;
+        this.usuario = usuario;
+    }
+
+    /* -----------------------------------------------metodos------------------------------------------------ */
+
+    private static String sha256(final String base) {
+        // fonte: https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha256-in-java
+        try{
+            final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final byte[] hash = digest.digest(base.getBytes("UTF-8"));
+            final StringBuilder hexString = new StringBuilder();
+            for (int i = 0; i < hash.length; i++) {
+                final String hex = Integer.toHexString(0xff & hash[i]);
+                if(hex.length() == 1) 
+                  hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        } catch(Exception ex){
+           throw new RuntimeException(ex);
+        }
+    }
+
+    public boolean depositar(String cpf, String senha, double valor){
+
+        if(validaUsuario(cpf, senha)){
+            this.saldo += valor;
+            return true;
+        }
+
+        return false;
+    }
+    public boolean sacar(String cpf, String senha, double valor){
+
+        if(validaUsuario(cpf, senha) && Double.compare(this.saldo, valor) >= 0){
+            this.saldo -= valor;
+            return true;
+        }
+
+        return false;
+    }
+        
+    
+    private boolean validaUsuario(String cpf, String senha){
+    if( cpf.equals(this.usuario.consultarCPF()) && usuario.validaSenha(senha)){
+    
+            return true;
+        }
+        return false;
+    }
+
+
+}
