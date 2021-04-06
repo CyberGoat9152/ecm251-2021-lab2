d0ed3b33d06d962e40209954338e007c521ba440
cybergoat9152 <arthurtbarros@outlook.com>
2021-04-05 22:37:08 -0300

    Remodelando a classe

--- trabalhos/Atividade01/src/barros/torres/arthur/Contas.java
+++ trabalhos/Atividade01/src/barros/torres/arthur/Contas.java
@@ -1,21 +1,36 @@
 import java.security.MessageDigest;
 
 public class Contas{
-    /* ----------------------------------------------atributos---------------------------------------------- */
+    /* 
+        Atributos da classe:
+            - idConta: (String)   Uma hash sha-256 feita com CPF (gerando um id unico para cada conta).
+            - saldo  : (Double)   Valor disponivel na conta corrente.
+            - usuario: (Usuarios) Usando a classe.
+        Metodo da classe:
+            - public boolean depositar(String cpf, String senha, double valor)   metodo para depositar dinheiro um boleano indicando se foi bem sucedido 
+            - public boolean sacar(String cpf, String senha, double valor)       metodo para sacar dinheiro retornando um boleano indicando se foi bem sucedido 
+            - private boolean validaUsuario(String cpf, String senha)            metodo para ver se o usuario e valido ou nao, caso nao seja, nao permite a criaçao da conta
+    */
 
-    private String idconta;
+
+    
+    /* ----------------------------------------------Atributos---------------------------------------------- */
+
+    private String idConta;
     private double saldo;
-    private Usuarios usuario;
+    protected Usuarios usuario;
   
-    /* ----------------------------------------------construtor--------------------------------------------- */
+    /* ----------------------------------------------Construtor--------------------------------------------- */
 
-    public Contas(Usuarios usuario){
-        this.idconta = sha256(usuario.consultarCPF()) ;
-        this.saldo = 0;
+    public Contas(Usuarios usuario,String senha, double saldo){
+        this.idConta = sha256(usuario.consultarCPF());
         this.usuario = usuario;
+        //System.out.println(this.idConta);
+        this.saldo = usuario.consultarIntegridadeDoUsuario() ? saldo : 0 ;
+
     }
 
-    /* -----------------------------------------------metodos------------------------------------------------ */
+    /* -----------------------------------------Metodo de hashing------------------------------------------- */
 
     private static String sha256(final String base) {
         // fonte: https://stackoverflow.com/questions/5531455/how-to-hash-some-string-with-sha256-in-java
@@ -35,15 +50,28 @@ public class Contas{
         }
     }
 
-    public boolean depositar(String cpf, String senha, double valor){
+    /* ----------------------------------------Metodos de consultas----------------------------------------- */
 
-        if(validaUsuario(cpf, senha)){
+    public String consultarIdConta(){
+        return this.idConta;
+    }
+
+
+    public double consultarSaldo(){
+        return this.saldo;
+    }
+
+    /* -----------------------------------------Metodos de manipulacao-------------------------------------- */
+    public boolean depositar(String cpf, String id, double valor){
+
+        if(validarConta(cpf, id)){
             this.saldo += valor;
             return true;
         }
 
         return false;
     }
+
     public boolean sacar(String cpf, String senha, double valor){
 
         if(validaUsuario(cpf, senha) && Double.compare(this.saldo, valor) >= 0){
@@ -53,15 +81,18 @@ public class Contas{
 
         return false;
     }
-        
-    
+    /* -----------------------------------------Metodos de validacao--------------------------------------------- */      
     private boolean validaUsuario(String cpf, String senha){
     if( cpf.equals(this.usuario.consultarCPF()) && usuario.validaSenha(senha)){
-    
             return true;
         }
         return false;
     }
+    private boolean validarConta(String cpf, String id){
+        boolean statusCPF = this.usuario.consultarCPF().equals(cpf);
+        boolean statusID = this.idConta.equals(id);
 
+        return statusCPF && statusID;
+    }
 
 }
